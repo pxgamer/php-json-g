6ccaedaac0e7a46b81a78365be715a5ce8c0744e
Owen Voke <development@voke.dev>
2022-02-14 20:29:00 +0000

    tests: update `JsonGFromImageTest` to Pest syntax

--- tests/JsonGFromImageTest.php
+++ tests/JsonGFromImageTest.php
@@ -1,31 +1,15 @@
 <?php
 
-namespace RaidAndFade\JsonG;
+use RaidAndFade\JsonG\JsonG;
 
-use function fopen;
-use Imagick;
-use ImagickException;
-use ImagickPixelException;
-use PHPUnit\Framework\TestCase;
+it('can convert image blob to JSON-G string', function () {
+    $testData = fopen(__DIR__.'/resources/demo.png', 'rb');
 
-class JsonGFromImageTest extends TestCase
-{
-    /**
-     * Test whether an image blob string can be converted to a JSON-G string.
-     *
-     * @throws ImagickPixelException
-     * @throws ImagickException
-     */
-    public function testCanConvertImageBlobToJsonGString(): void
-    {
-        $testData = fopen(__DIR__.'/resources/demo.png', 'rb');
+    $image = new Imagick();
+    $image->readImageFile($testData);
 
-        $image = new Imagick();
-        $image->readImageFile($testData);
+    $json = JsonG::toJson($image);
 
-        $json = JsonG::toJson($image);
-
-        $this->assertJson($json);
-        $this->assertJsonStringEqualsJsonFile(__DIR__.'/resources/demo.jsng', $json);
-    }
-}
+    expect($json)->toBeJson();
+    $this->assertJsonStringEqualsJsonFile(__DIR__.'/resources/demo.jsng', $json);
+});
